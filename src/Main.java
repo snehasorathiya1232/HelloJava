1b276934809bed5fb8a4f2cd0661a64c36d46bbf
unknown <snehasorathiya1@gmail.com>
2026-05-17 19:57:08 +0530

    learn student

--- src/Main.java
+++ src/Main.java
@@ -14,11 +14,15 @@ public class Main {
         System.out.println("enter sub 2: ");
         int s2 = sc.nextInt();
 
-        int c = s1 + s2;
+        int sum = s1 + s2;
+        int per = sum / 2;
         System.out.println("Name: "+name);
         System.out.println("age: "+age);
+        System.out.println(age > 18);// answer in true or false
         System.out.println("sub 1: "+s1);
         System.out.println("sub 2: " +s2);
-        System.out.println("total: "+c);
+        System.out.println("total: "+sum);
+        System.out.println("persentage: "+per);
+
     }
 }
