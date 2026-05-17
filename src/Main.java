04361186c27d4317fe10181a79f9df1e0bbfc696
unknown <snehasorathiya1@gmail.com>
2026-05-17 18:17:55 +0530

    learn variable

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        String name="sneha";
+        int age = 19;
+        double rupee=12321.22;
+
         System.out.println("hello everyone i learn git");
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(rupee);
     }
 }
