be77cd6c0af2c286b5e7b9b0d43cdd6f8a7ae8ac
unknown <snehasorathiya1@gmail.com>
2026-05-17 21:00:38 +0530

    plus student

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,49 @@ public class Main {
         System.out.println("enter name: ");
         String name = sc.nextLine();
 
+        System.out.println("enter SPID: ");
+        String id = sc.nextLine();
+
+        System.out.println("enter password: ");
+        String pass = sc.nextLine();
+
         System.out.println("enter age: ");
         int age = sc.nextInt();
 
-        System.out.println("enter sub 1: ");
-        int s1 = sc.nextInt();
+        if(pass.equals("Sneha@2006")) {
+            System.out.println("");
+            System.out.println("LOGIN SUCESSFULLL");
+            System.out.println("");
+            System.out.println("enter sub 1: ");
+            double s1 = sc.nextInt();
 
-        System.out.println("enter sub 2: ");
-        int s2 = sc.nextInt();
+            System.out.println("enter sub 2: ");
+            double s2 = sc.nextInt();
 
-        int sum = s1 + s2;
-        int per = sum / 2;
-        System.out.println("Name: "+name);
-        System.out.println("age: "+age);
-        System.out.println(age > 18);// answer in true or false
-        System.out.println("sub 1: "+s1);
-        System.out.println("sub 2: " +s2);
-        System.out.println("total: "+sum);
-        System.out.println("persentage: "+per);
+            System.out.println("enter sub 3: ");
+            double s3 = sc.nextInt();
+
+            double sum = s1 + s2 + s3;
+            double per = (sum / 150) * 100;
+            System.out.println("-------------------------------");
+            System.out.println("Name: " + name);
+            System.out.println("age: " + age);
+            System.out.println(age > 18);// answer in true or false
+            System.out.println("sub 1: " + s1);
+            System.out.println("sub 2: " + s2);
+            System.out.println("sub 3: " + s3);
+            System.out.println("-------------------------------");
+            System.out.println("*total 50 mark per paper*");
+            System.out.println("total: " + sum);
+            System.out.println("percentage: " + per);
+            if (per > 40) {
+                System.out.println("status: pass");
+            } else {
+                System.out.println("status: fail");
+            }
+        }else{
+            System.out.println("LOGIN FAIL!! WRONG PASSWORD");
+        }
 
     }
 }
